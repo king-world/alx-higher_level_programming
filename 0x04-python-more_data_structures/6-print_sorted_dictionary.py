6ef9486545a96af739b7cd8c975c313d4767a365
Ubong Obot <obot.kingsley23@gmail.com>
2024-02-09 05:15:37 -0800

    Adding task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
+    for i in sorted(a_dictionary.keys()):
+        print("{}: {}".format(i, a_dictionary.get(i)))
